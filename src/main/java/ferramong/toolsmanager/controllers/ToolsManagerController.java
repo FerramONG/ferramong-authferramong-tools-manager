8687f1f14a44054a51ee9abd771e9c286ac89910
Renan Kummer <dev.renankummer@outlook.com>
2021-03-25 22:44:58 -0300

    :memo: Document GET /search/tools/{toolName} API with OpenAPI

--- src/main/java/ferramong/toolsmanager/controllers/ToolsManagerController.java
+++ src/main/java/ferramong/toolsmanager/controllers/ToolsManagerController.java
@@ -3,7 +3,12 @@ package ferramong.toolsmanager.controllers;
 import ferramong.toolsmanager.entities.Tool;
 import ferramong.toolsmanager.models.ToolsListFilter;
 import ferramong.toolsmanager.services.ToolsManagerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +22,17 @@ public class ToolsManagerController {
 
     private final ToolsManagerService toolsManagerService;
 
-    @GetMapping("/search/{toolName}/tools")
-    public List<Tool> searchTools(@PathVariable("toolName") @NotBlank String toolName) {
+    @Operation(summary = "Search tools by name.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved tools containing searched name.")
+    })
+    @GetMapping(value = "/search/tools/{toolName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Tool> searchTools(
+            @Parameter(description = "The name to search for", required = true, example = "hammer")
+            @PathVariable("toolName")
+            @NotBlank
+                    String toolName
+    ) {
         var filter = ToolsListFilter.builder()
                 .toolName(toolName)
                 .build();
