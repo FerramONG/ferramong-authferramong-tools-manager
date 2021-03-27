fb1bf0d7b95d97c63e8105533c4bca7aaf8ef616
Renan Kummer <dev.renankummer@outlook.com>
2021-03-26 22:00:47 -0300

    :sparkles: Enable cross-origin requests

--- src/main/java/ferramong/toolsmanager/controllers/ToolsManagerController.java
+++ src/main/java/ferramong/toolsmanager/controllers/ToolsManagerController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @AllArgsConstructor
 public class ToolsManagerController {
 
