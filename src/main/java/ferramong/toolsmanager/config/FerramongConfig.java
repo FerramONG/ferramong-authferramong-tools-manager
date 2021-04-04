98454f0ed451313ff6d89d64790fc70be50c7cd7
Renan Kummer <dev.renankummer@outlook.com>
2021-04-04 02:02:41 -0300

    :wrench: Reorganize configuration properties

---
+++ src/main/java/ferramong/toolsmanager/config/FerramongConfig.java
@@ -0,0 +1,16 @@
+package ferramong.toolsmanager.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class FerramongConfig {
+
+    @Value("${ferramong.tools-manager.base-url}")
+    private String toolsManagerBaseUrl;
+
+    @Value("${ferramong.scheduler.base-url}")
+    private String schedulerBaseUrl;
+}
