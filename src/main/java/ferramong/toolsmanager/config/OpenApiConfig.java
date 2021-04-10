35c324de70eb74bc0b77078f047d42df6e19b59a
Renan Kummer <dev.renankummer@outlook.com>
2021-04-10 20:09:27 -0300

    :memo: Update API name

--- src/main/java/ferramong/toolsmanager/config/OpenApiConfig.java
+++ src/main/java/ferramong/toolsmanager/config/OpenApiConfig.java
@@ -3,5 +3,5 @@ package ferramong.toolsmanager.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@OpenAPIDefinition(info = @Info(title = "FerramONG Tools Manager API"))
+@OpenAPIDefinition(info = @Info(title = "FerramONG Tools Manager"))
 public class OpenApiConfig { }
